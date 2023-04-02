b101453e7407fee6d0ef000022a1f84c35f11600
jaks03 <renault12003@mail.ru>
2023-04-02 21:24:33 +0300

    добавлена кнопка 3

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.Button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Button1
@@ -37,15 +39,36 @@
             this.Button1.Name = "Button1";
             this.Button1.Size = new System.Drawing.Size(150, 113);
             this.Button1.TabIndex = 0;
-            this.Button1.Text = "Кнопочка_1";
+            this.Button1.Text = "Главная";
             this.Button1.UseVisualStyleBackColor = true;
             this.Button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(482, 101);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 130);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Кнопка 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(249, 321);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 76);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Кнопка 3";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1215, 584);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.Button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +79,8 @@
         #endregion
 
         private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
